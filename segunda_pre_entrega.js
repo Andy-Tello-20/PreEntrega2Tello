a29f56f2715dd2369133acd3ee698cfe4d1572c4
Andres Tello <andrest911@gmail.com>
2023-06-19 10:16:24 -0300

    Nueva opcion, buscar por ventas

--- segunda_pre_entrega.js
+++ segunda_pre_entrega.js
@@ -4,11 +4,11 @@ function confirmarAccion() {
     if (respuesta) {
 
         class moldeRegistro {
-            constructor(id, ventas, interes,salidas) {
+            constructor(id, ventas, interes, salidas) {
                 this.id = id
                 this.ventas = ventas,
-                this.interes = interes,
-                this.salidas= salidas
+                    this.interes = interes,
+                    this.salidas = salidas
             }
 
             vistaPrevia() {
@@ -17,17 +17,17 @@ function confirmarAccion() {
         }
 
         let listaRegistros = []
-        let contador=0
+        let contador = 0
 
 
         function registrar() {
-            contador+=1
+            contador += 1
             let Id = contador
             let ventas = parseInt(prompt("Ingrese una nueva venta:"))
             let interes = parseInt(prompt("Ingrese un % de interes"))
-            let salidas = parseInt(prompt("Ingrese Salidas/Compras realizadas:") )
+            let salidas = parseInt(prompt("Ingrese Salidas/Compras realizadas:"))
 
-            let nuevoRegistro = new moldeRegistro(Id, ventas, interes,salidas)
+            let nuevoRegistro = new moldeRegistro(Id, ventas, interes, salidas)
 
             listaRegistros.push(nuevoRegistro)
 
@@ -57,7 +57,32 @@ function confirmarAccion() {
 
         }
 
-        function registroCompleto() {$
+        function BuscarPorVentas() {
+            let minimo = parseInt(prompt("Ingrese una venta minima:"))
+            let maximo = parseInt(prompt("Ingrese una venta Maxima:"))
+
+            let rango = listaRegistros.filter(
+                (i) => i.ventas >= minimo && i.ventas <= maximo
+            )
+
+            //Condicion: La longitud de la nueva lista "rango" segun los elementos encontrados por el metodo FILTER tienen que ser mayor a 0
+            if (rango.length > 0) {
+
+                let cantidadEncontrada = `Se encontraron ${rango.length} Registros\n\n`
+
+                //Se crea una nueva lista "resultado" y se la transforma concatenando texto + elementos de la misma lista
+                let resultados = rango.map(
+                  (i) => `Id: ${i.id}, Ventas: $${i.ventas}, Interés: $${i.interes}, Salidas: $${i.salidas}`
+                )
+            
+                let mensaje = resultados.join("\n\n")
+                alert(`${cantidadEncontrada}${mensaje}`)
+              } else {
+                alert("No se encontraron registros en el rango especificado")
+              }
+        }
+
+        function registroCompleto() {
 
             let iterarTodo = listaRegistros.map(
                 (i) => `ID: ${i.id}, Ventas: $${i.ventas}, Interes: $${i.interes}, Salidas: $${i.salidas}`
@@ -109,7 +134,7 @@ function confirmarAccion() {
             let menuExit = true
 
             do {
-                let menuOpciones = parseInt(prompt(`Ingrese la opción deseada\n\n1) Registrar diario de contabilidad\n2) Ver registro\n3) Ver todos los registros\n4) Eliminar un registro\n5) Ver resumen de Contabilidad\n6) Salir del menu`))
+                let menuOpciones = parseInt(prompt(`Ingrese la opción deseada\n\n1) Registrar diario de contabilidad\n2) Ver registro\n3) Ver todos los registros\n4) Buscar por rango de ventas\n5) Eliminar un registro\n6) Ver resumen de Contabilidad\n7) Salir del menu`))
                 switch (menuOpciones) {
                     case 1:
                         registrar()
@@ -121,9 +146,12 @@ function confirmarAccion() {
                         registroCompleto()
                         break
                     case 4:
+                        BuscarPorVentas()
+                        break
+                    case 5:
                         borrarRegistro()
                         break
-                    case 6:
+                    case 7:
                         console.log(`Gracias por utilizar nuestra app. Saludos!`)
                         menuExit = false
                         break
